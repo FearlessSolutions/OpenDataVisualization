5d308b23d20154cb13d934077acce25a8b276791
Rebecca Spencer-Strong <beck@rd-MacBookPro.localdomain>
2019-09-23 11:26:48 -0400

    added missing semi colon

--- opendatavisualization/src/App.js
+++ opendatavisualization/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import MapContainer from "./components/MapContainer/mapContainer";
 import Header from "./components/header";
-import Footer from "./components/footer"
+import Footer from "./components/footer";
 
 function App (){
         return (
